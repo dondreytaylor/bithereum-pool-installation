afc11731b40e9dfe116c9ec2611b549b51fc6d42
Dondrey Taylor <dondrey.taylor@gmail.com>
2018-09-16 01:37:41 -0400

    Pool installation script added

---
+++ install.sh
@@ -0,0 +1,8 @@
+# Installation for Ubuntu 18.04
+sudo apt-get update -y
+sudo apt-get install npm -y
+sudo npm install n -g -y
+sudo n v7
+
+git clone https://github.com/dondreytaylor/btg-node-stratum-pool.git
+cd btg-node-stratum-pool
